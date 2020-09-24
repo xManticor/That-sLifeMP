d83f2c10950a6a9ede432142db11c304e4d12e6e
kyle <lolkyle2001@gmail.com>
2020-09-24 15:02:56 +0800

    changed button text to ETITS

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -35,7 +35,7 @@ public class Main extends Application {
             System.out.println("YUM!");
         });
 
-        button.setText("HATDOG");
+        button.setText("ETITS");
         primaryStage.show();
     }
 
