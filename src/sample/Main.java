066aa2c71d31ed4f1c709ef00dc764561893a89f
Toasted <Toasted@users.noreply.github.com>
2020-09-24 14:32:32 +0800

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
+
+    @FXML
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        button = new Button();
+
+       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("That's Life!");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 300, 250);
+
+        primaryStage.setScene(scene);
+        layout.getChildren().add(button);
+        button.setOnAction(e->{
+            System.out.println("YUM!");
+        });
+
+        button.setText("HATDOG");
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
