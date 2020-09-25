ebc87d9ed44974a9c485ba820156c25b95872a19
jjjjjx <judsdeleonx@gmail.com>
2020-09-25 17:10:27 +0800

    try

--- out/production/Game/model/Path.java
+++ out/production/Game/model/Path.java
@@ -2,6 +2,23 @@ import java.util.*;
 
 public class Path
 {
+    private String name;
+    private Space[] spaces;
+    private int endpath;
 
-    public Path()
+    public Path(String name, int numspaces)
+    {
+        this.name = name;
+        spaces = new Space[numspaces];
+    }
+
+    public spaces getSpaces()
+    {
+        return spaces;
+    }
+
+    public String getSpaceType()
+    {
+        return space NEED INDEX
+    }
 }
